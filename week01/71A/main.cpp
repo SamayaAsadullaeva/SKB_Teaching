13fb616ce943879f383875acde4b5c05c7d49c6a
SamayaAsadullaeva <40027355+SamayaAsadullaeva@users.noreply.github.com>
2019-04-10 13:43:08 +0300

    Update main.cpp

--- week01/71A/main.cpp
+++ week01/71A/main.cpp
@@ -1,13 +1,23 @@
-#include <QCoreApplication>
 #include <iostream>
-
+#include <string>
+#include <stdlib.h>
 using namespace std;
 
-int main(int argc, char *argv[])
+int main()
 {
-    QCoreApplication a(argc, argv);
+    string word;
+    int n, length;
+    cin>>n;
+    for (int i=0;i<n;i++){
+        cin>>word;
+    length= word.length();
+    if (length>10){
+        cout<< word[0]<< length-2<<word[length-1]<<endl;
+    };
+        
+    }
+    
+    system("pause");
 
-    cout << "Hello world!" << endl;
-
-    return a.exec();
+    return 0;
 }
